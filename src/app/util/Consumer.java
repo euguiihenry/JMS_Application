c395bacfb510edd8181fdf02bb1a9e8a90f55601
Henry <gui2012451@gmail.com>
2023-06-12 17:05:40 -0300

    Build: Building Consumer and inserting some methods into Link Class

--- src/app/util/Consumer.java
+++ src/app/util/Consumer.java
@@ -1,1 +1,66 @@
 package app.util;
+
+import javax.jms.*;
+import app.view.Error;
+
+public class Consumer extends Link implements Runnable {
+	/* Global Variables:
+	=====================================================================*/
+		Link linkObj;
+		Topic topic;
+		MessageConsumer consumer;
+		Error errorObj;
+		
+	/* Constructor:
+	=====================================================================*/
+		public Consumer() {
+			this.topic = this.linkObj.getTopic();
+			try {
+				this.consumer = this.linkObj.getSession().createConsumer(this.topic);
+				
+			} catch (JMSException e) {
+				this.errorObj.showError(e);
+				e.printStackTrace();
+			}
+		}
+		
+	/* Methods:
+	=====================================================================*/
+		/* Create Consumer:
+		=================================================================*/
+			public MessageConsumer createConsumer() {
+				try {
+					this.consumer = this.linkObj.getSession().createConsumer(this.topic);
+					return this.consumer;
+					
+				} catch (JMSException e) {
+					this.errorObj.showError(e);
+					e.printStackTrace();
+					return null;
+				}
+			}
+		
+		/* Message Listener:
+		=================================================================*/
+			public void run() {
+				
+				/*try {
+					this.consumer.setMessageListener(new MessageListener() {
+						
+						public void onMessage(Message message) {
+							if (message instanceof TextMessage) {
+	                            TextMessage textMessage = (TextMessage) message;
+	                            String text;
+	                            text = textMessage.getText();
+								System.out.println("Received message: " + text);
+	                            
+	                        }
+						}
+					});
+				} catch (JMSException e) {
+					this.errorObj.showError(e);
+					e.printStackTrace();
+				}*/
+			}
+			
+}
--- src/app/util/Link.java
+++ src/app/util/Link.java
@@ -5,7 +5,7 @@ import org.apache.activemq.*;
 import app.view.Error;
 
 public class Link extends Error{
-	/* Global Variables:
+	/* Global Variables: (The final variables displayed below should be inserted in an TXT file to be change when need without the need to restart the system.) 
 	=========================================================================================*/
 		private final String protocol = "tcp";
 		private final String address = "127.0.0.1";
@@ -13,6 +13,7 @@ public class Link extends Error{
 		private ConnectionFactory connectionFactory;
 		private Connection connection;
 		private Session session;
+		private Topic topic;
 		private Error error;
 		
 	/* Constructor:
@@ -23,50 +24,92 @@ public class Link extends Error{
 	
 	/* Methods:
 	=========================================================================================*/
-		public void connect() {
-			this.startConnection();
-			this.startSession();
-		}
+		/* Main Method to Run Connections:
+		=====================================================================================*/
+			public void connect() {
+				this.startConnection();
+				this.startSession();
+				this.startTopic();
+			}
 		
-		public void startConnection() {
-			// Create a connection factory (Check docs for more info):
-				this.connectionFactory = new ActiveMQConnectionFactory(protocol + "://" + address + ":" + port);
+		/* Connection Factory:
+		=====================================================================================*/
+			public void startConnection() {
+				// Create a connection factory (Check docs for more info):
+					this.connectionFactory = new ActiveMQConnectionFactory(protocol + "://" + address + ":" + port);
+				
+				// Create a connection and starting it (Check docs for more info):
+					try {
+						this.connection = this.connectionFactory.createConnection();
+						connection.start();
+						
+					} catch (JMSException e) {
+						this.error.showError(e);
+						e.printStackTrace();
+					}
+					
+			}
 			
-			// Create a connection and starting it (Check docs for more info):
+		/* Starting Session:
+		=====================================================================================*/
+			public void startSession() {
 				try {
-					this.connection = this.connectionFactory.createConnection();
-					connection.start();
+					this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 					
 				} catch (JMSException e) {
 					this.error.showError(e);
 					e.printStackTrace();
 				}
-				
-		}
-		
-		public void startSession() {
-			try {
-				this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-				
-			} catch (JMSException e) {
-				this.error.showError(e);
-				e.printStackTrace();
 			}
-		}
+		/* Starting Topic:
+		=====================================================================================*/
+			public void startTopic() {
+				try {
+					this.topic = this.session.createTopic("mainRoom");
+					
+				} catch (JMSException e) {
+					this.error.showError(e);
+					e.printStackTrace();
+				}
+			}
 		
-		public void specificConnection() {
+		/* Starting Queue for Specific Destination:
+		=====================================================================================*/
+			public Queue specificConnection() {
+				try {
+					Queue specificDestination = this.session.createQueue("specificDest");
+					return specificDestination;
+					
+				} catch (JMSException e) {
+					this.error.showError(e);
+					e.printStackTrace();
+					return null;
+				}
+			}
 			
-		}
+		/* Get Session:
+		=====================================================================================*/
+			public Session getSession() {
+				return this.session;
+			}
+			
+		/* Get Topic Destination:
+		=====================================================================================*/
+			public Topic getTopic() {
+				return this.topic;
+			}
 		
-		public void setClient() {
+		/* Probably The Methods Below Will Not Be Necessary in Here:
+		=====================================================================================*/
+			public void setClient() {
+				
+			}
 			
-		}
-		
-		public void getClients() {
+			public void getClients() {
+				
+			}
 			
-		}
-		
-		public void getClient(String username) {
-			
-		}
+			public void getClient(String username) {
+				
+			}
 }
